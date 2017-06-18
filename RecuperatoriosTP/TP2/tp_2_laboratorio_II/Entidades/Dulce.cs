a012d2ea1cf7ded79366cc6d6d19bbe5f9adb8a6
Daniel Ruben de Brito <danieldebrito@outlook.com>
2017-06-18 20:30:09 -0300

    RecuperatoriosTP Daniel de Brito 2D
    RecuperatoriosTP Daniel de Brito 2D
    
    TP1  - no entregado en fecha
    TP2  - con corecciones

---
+++ RecuperatoriosTP/TP2/tp_2_laboratorio_II/Entidades/Dulce.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Dulce: Producto
+    {
+        #region constructores
+
+        public Dulce(EMarca marca, string codigo, ConsoleColor color)
+            : base(marca, codigo, color)
+        {
+        }
+
+        #endregion
+
+        #region propiedades
+
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        public override short CantidadCalorias
+        {
+            get { return 80;}
+        }
+
+        #endregion
+
+        #region metodos
+        
+        /// <summary>
+        /// Retorna toda la informacion del Producto.
+        /// </summary>
+        /// <returns></returns>
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("CALORIAS : {0}", this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
---
+++ RecuperatoriosTP/TP2/tp_2_laboratorio_II/Entidades/Producto.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public abstract class Producto
+    {
+        #region enumerado
+        
+        public enum EMarca { Serenisima, Campagnola, Arcor, Ilolay, Sancor, Pepsico }
+        
+        #endregion
+
+        #region atributos
+        
+        protected EMarca _marca;
+        protected string _codigoDeBarras;
+        protected ConsoleColor _colorPrimarioEmpaque;
+        
+        #endregion
+
+        #region constructor
+        
+        protected Producto(EMarca marca, string codeBar, ConsoleColor color)
+        {
+            this._marca = marca; 
+            this._codigoDeBarras = codeBar;
+            this._colorPrimarioEmpaque = color;
+        }
+        
+        #endregion
+
+        #region propiedades
+       
+        /// <summary>
+        /// ReadOnly: Retornará la cantidad de calorias del producto
+        /// </summary>
+        public abstract short CantidadCalorias { get; }
+        
+        #endregion
+       
+        #region metodos
+
+        /// <summary>
+        /// Publica todos los datos del Producto.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar()
+        {
+            return (string)this;
+        }
+        
+        #endregion
+
+        #region operadores
+        
+        /// <summary>
+        /// Retorna toda la informacion del producto.
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        public static explicit operator string (Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendFormat("CODIGO DE BARRAS: {0}\r\n", p._codigoDeBarras);
+            sb.AppendFormat("MARCA          : {0}\r\n", p._marca.ToString());
+            sb.AppendFormat("COLOR EMPAQUE  : {0}\r\n", p._colorPrimarioEmpaque.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+       
+        /// <summary>
+        /// Dos productos son iguales si comparten el mismo código de barras
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator == (Producto v1, Producto v2)
+        {
+            return (v1._codigoDeBarras == v2._codigoDeBarras);
+        }
+        /// <summary>
+        /// Dos productos son distintos si su código de barras es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator != (Producto v1, Producto v2)
+        {
+            return !(v1 == v2);
+        }
+       
+        #endregion
+    }
+}
